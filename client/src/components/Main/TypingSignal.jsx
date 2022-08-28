42ff2a7de93f860b01ccbc57c85f6024711f65e7
jah <jonjoh121@gmail.com>
2022-08-27 21:21:22 -0700

    modify color

--- client/src/components/Main/TypingSignal.jsx
+++ client/src/components/Main/TypingSignal.jsx
@@ -20,7 +20,7 @@ function TypingSignal({children}) {
 	return (
 		<span className={classes.root}>
 			{children}
-			<Preloader use={ThreeDots} size={32} strokeWidth={12} strokeColor='#b9b9b9' duration={800} /> 
+			<Preloader use={ThreeDots} size={32} strokeWidth={12} strokeColor='pink' duration={800} /> 
 		</span>
 	)
 }
