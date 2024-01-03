98ca4f48456108ac366beb84ee425fc28bc6aebd
jahdevelops <elijaheze777@gmail.com>
2024-01-02 19:19:21 -0600

    configure build script

---
+++ build_app.sh
@@ -0,0 +1,16 @@
+#!/bin bash
+
+echo "--- Install client deps ---"
+cd client && npm i --legacy-peer-deps
+echo -e "n\n\n"
+
+echo "--- Build client with webpack ---"
+npm run build
+echo -e "n\n\n"
+
+echo "--- Install server deps ---"
+cd ../server && npm i 
+echo -e "n\n\n"
+
+echo "--- Start server ---"
+pm2 start ecosystem.config.js --port 4200
