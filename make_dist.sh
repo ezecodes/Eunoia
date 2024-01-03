f43f193b6cdab9d3c85ae491b8b12b6e1eb32151
jahdevelops <elijaheze777@gmail.com>
2024-01-02 19:06:34 -0600

    enable pm2

--- make_dist.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd client && npm i --legacy-peer-deps && npm run build
---
+++ server/ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'eunoia_cluster',
+      script: 'index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: false,
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      env: {
+        NODE_ENV: 'production',
+      },
+     
+      error_file: '/tmp/eunoia_cluster_logs/error.log',
+      out_file: '/tmp/eunoia_cluster_logs/out.log',
+     
+      log_rotate_date: true,
+      log_max_size: '100M',
+      log_max_files: 10,
+    },
+  ],
+};
